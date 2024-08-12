1feaae02e50b8751a31a94c27c2fc3b09656886e
ramkumar gurav <ramkumarsgurav@gmail.com>
2024-08-12 20:06:03 +0530

    asdf

---
+++ application/views/home.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+  <title>"Forgot Password for your"</title>
+</head>
+
+<body
+  style="width:100%; font-family:Arial; font-size:13px; line-height:22px; background:#fff;  position:relative;color:#555555; margin:0px; padding:0px;">
+  <div style="margin:0 auto; width:600px;">
+    <div
+      style="background-color: #5f9116;; width:594px; float:left; padding:0px 3px 3px 3px; border:#5f9116 3px solid;">
+      <div style="padding:27px; width:540px; border-bottom:#5f9116 3px solid;">
+        <a href="http://localhost/xampp/MARS/ci3_id_name/secureRegions/" target="_blank"
+          style="font-size:13px; color:#839ecd;text-decoration:none; text-align:center;">
+          <img src="http://localhost/xampp/MARS/ci3_id_name/secureRegions//admin/images/email-logo.png" alt="" /></a>
+
+      </div>
+      <div style="background:#fff; padding:44px 34px; width:526px; float:left;">
+        <h1 style="color:#d95451; font-size:24px; font-family:Arial; font-weight:normal; margin-bottom:20px;">Reset
+          Password Link</h1>
+        <p style="margin-bottom:20px;">
+          If you want to reset your password, click here the following link</p>
+        <p style="margin-bottom:10px;"><a
+            href="http://localhost/xampp/MARS/ci3_id_name/secureRegions/Login/reset_password/ZYRXK9JXQ4JI">Reset
+            Password</a></p>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
